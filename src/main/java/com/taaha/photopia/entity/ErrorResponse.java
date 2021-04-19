82cf71c8cff096236f8b4acd57a40fd67612b28f
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-19 18:57:19 +0600

    errorresponse entity done

---
+++ src/main/java/com/taaha/photopia/entity/ErrorResponse.java
@@ -0,0 +1,53 @@
+package com.taaha.photopia.entity;
+
+import java.util.Date;
+
+public class ErrorResponse {
+
+    private Date timestamp;
+    private int status;
+    private String message;
+    private String details;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(Date timestamp, int status, String message, String details) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
